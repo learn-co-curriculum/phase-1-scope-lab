6e20ba9659f6f4c0f908975183917c4c50d873b4
Gabe Jackson <gj@mail.co.de>
2017-09-25 18:20:28 -0400

    Use anonymous functions instead of arrow functions since they won't look quite so foreign to students looking at the test suite

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,42 +1,48 @@
 const expect = chai.expect;
-describe('Fix the Scope', function() {
-  describe('declare customerName to be bob in global scope', function() {
-    it('returns the customerName', () => {
-      expect(window.customerName).to.equal('bob')
-    })
-  })
 
-  describe('upperCaseCustomerName()', function() {
-    it('returns the customerName', () => {
-      expect(window.customerName).to.equal('bob')
-      upperCaseCustomerName()
-      expect(window.customerName).to.equal('BOB')
-    })
-  })
-  describe('setBestCustomer()', function() {
-    it('setBestCustomer', () => {
-      expect(window.bestCustomer).to.equal(undefined)
-      window.setBestCustomer()
-      expect(window.bestCustomer).to.equal('not bob')
-    })
-  })
+describe('Fix the Scope', function () {
+  describe('declare customerName to be bob in global scope', function () {
+    it('returns the customerName', function () {
+      expect(window.customerName).to.equal('bob');
+    });
+  });
 
-  describe('overWriteBestCustomer()', function() {
-    it('overwrites the best customer', () => {
-      overWriteBestCustomer('maybe bob')
-      expect(window.bestCustomer).to.equal('maybe bob')
-    })
-  })
+  describe('upperCaseCustomerName()', function () {
+    it('returns the customerName', function () {
+      expect(window.customerName).to.equal('bob');
 
-  describe('reassignLeastFavorite', function(){
-    it('unsuccessfully tries to reassign the least favorite customer', () => {
-      expect(changeLeastFavoriteCustomer).to.throw(TypeError)
-    })
-  })
+      upperCaseCustomerName();
 
-  describe('attemptTwoFavoriteCustomers', function(){
-    it('unsuccessfully tries to declare favoriteCustomer with let two times', () => {
-      expect(attemptTwoFavoriteCustomers).to.throw(SyntaxError)
-    })
-  })
-})
+      expect(window.customerName).to.equal('BOB');
+    });
+  });
+  describe('setBestCustomer()', function () {
+    it('setBestCustomer', function () {
+      expect(window.bestCustomer).to.equal(undefined);
+
+      window.setBestCustomer();
+
+      expect(window.bestCustomer).to.equal('not bob');
+    });
+  });
+
+  describe('overwriteBestCustomer()', function () {
+    it('overwrites the best customer', function () {
+      overwriteBestCustomer('maybe bob');
+
+      expect(window.bestCustomer).to.equal('maybe bob');
+    });
+  });
+
+  describe('reassignLeastFavorite()', function () {
+    it('unsuccessfully tries to reassign the least favorite customer', function () {
+      expect(changeLeastFavoriteCustomer).to.throw(TypeError);
+    });
+  });
+
+  describe('attemptTwoFavoriteCustomers()', function () {
+    it('unsuccessfully tries to declare favoriteCustomer with let two times', function () {
+      expect(attemptTwoFavoriteCustomers).to.throw(SyntaxError);
+    });
+  });
+});
