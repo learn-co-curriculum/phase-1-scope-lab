76cac1945a9779ab4836cfead249129dcc35a0db
Gabe Jackson <gj@mail.co.de>
2017-09-25 18:22:34 -0400

    Add solution with updated formatting

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+
+const leastFavoriteCustomer = 'sour fred';
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase();
+}
+
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'sour fred';
+}
+
+function setBestCustomer () {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer (newFavorite) {
+  bestCustomer = newFavorite;
+}
+
+function congratulateCustomer () {
+  return `congrats ${favoriteCustomer}`;
+}
+
+function attemptTwoFavoriteCustomers () {
+  let favoriteCustomer = 'bar';
+  let favoriteCustomer = 'not bar';
+}
