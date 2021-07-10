d8ce3be668d04a047f7dc74e78e292e99179b0dc
Ajmol Uddin <uddin.ajmol@gmail.com>
2021-07-10 00:20:42 -0400

    commit tree d5945b3b82b8bf5b4dc476ea61c581475821ed80

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+  return customerName;
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+  return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+  return bestCustomer;
+}
+
+const leastFavoriteCustomer = 'Ajmol';
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'Not going to work';
+  return leastFavoriteCustomer;
+}
