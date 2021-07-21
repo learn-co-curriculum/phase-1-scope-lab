3c86d7a0c616fe984541ad107d3e25e81de5ed4b
TrevorJohnson <trevor.assetsolutions@gmail.com>
2021-07-21 11:10:54 -0600

    commit tree b886e8c3e8fe5ac919d4111dbc6a1e322f2bf6bc

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+var customerName = 'bob'
+//global var customerName
+var bestCustomer;
+//global best customer
+
+function upperCaseCustomerName(){
+   
+    customerName = customerName.toUpperCase() 
+
+}
+
+function setBestCustomer() {
+     bestCustomer = 'not bob'
+//scope bestCustomer not bob    
+}
+
+function overwriteBestCustomer(){
+
+    bestCustomer = 'maybe bob'
+    //scope chain overwritten previous function
+}
+
+function changeLeastFavoriteCustomer(){
+
+    const leastFavoriteCustomer = "jeff"
+
+    leastFavoriteCustomer = customerName;
+}
