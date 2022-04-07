8ef649ddbdaff5ba0efab72a740d0efd5f0497a8
Alex Kiborgok <borgokalek14@gmail.com>
2022-04-07 11:01:48 +0300

    add scope lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Write your solution in this file!
+customerName = "bob";
+bestCustomer;
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer(customer) {
+    bestCustomer = customer
+}
+
+function changeLeastFavoriteCustomer() {
+    const leastFavouriteCusomer = 'somebody'
+    leastFavouriteCusomer = "somebodyelse";
+}
