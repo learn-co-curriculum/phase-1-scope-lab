17d289c610b3c2468401bd6732c4773b4023ea53
DESKTOP-SAWA\ASUS <eng.baraahmassri@gmail.com>
2023-08-17 08:39:28 +0300

    phase-1-scope-lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = "maybe bob";
+}
+
+const leastFavoriteCustomer = "someone";
+
+function changeLeastFavoriteCustomer() {
+  // This will result in an error as you cannot reassign a constant.
+  leastFavoriteCustomer = "someone else";
+}
